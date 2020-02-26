80130d89993259c5bd74a1e778a0052bd6cd528d
ambernina <rob.ambernina@gmail.com>
2020-02-26 12:59:19 -0700

    fixed stuff

--- server.js
+++ server.js
@@ -4,21 +4,23 @@ const path = require('path');
 const fs = require('fs');
 
 // set up the express app
-const app = express();
+let app = express();
 const PORT = process.env.PORT || 3000;
 
 // handles the data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
-const writeData = function(){
+let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+console.log(data);
+
+const writeNote = function(){
 	fs.writeFile("./db/db.json", JSON.stringify(data), err => {
 		if (err) throw err;
 	});
 };
 
-let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-console.log(data);
 
 
 app.get('/', function(req, res) {
@@ -31,26 +33,38 @@ app.get('/notes', function(req, res) {
 
 // grabs info from the notes file(?) and posts to page
 app.post('/api/notes', function(req, res) {
-	data.push(req.body);
-	console.log(data);
-	writeData();
+	let newNote = req.body;
+	if (data.length === 0) {
+		let id = 1;
+	} else {
+		let id = parseInt(data[data.length - 1].id) + 1;
+	}
+	newNote.id = id;
+	data.push(newNote);
+	console.log(newNote);
+	writeNote();
 	return res.json(data);
 });
 
 app.get('/api/notes', function(req, res) {
-	return res.json(data);
+	fs.readFile('.db/db.json', 'utf8', (err, data) => {
+		console.log(data);
+		if (err) throw err;
+		const note = JSON.parse(data);
+		return res.json(data);
+	})
 });
 
 app.delete('/api/notes/:id', function(req, res) {
-	const id = req.params.id;
-	data = data.filter(function(note) {
-		if (note.id === id) {
-			return false;
+	let newData = [];
+	for (var i = 0; i < data.length; i++) {
+		if(data[i].id !== parseInt(req.params.id)) {
+			newData.push(data[i]);
 		}
-		return true;
-	});
-	writeData();
-	return res.json(data);
+	}
+	data = newData;
+	writeNote();
+	res.json(data);
 })
 
 app.listen(PORT, function() {
